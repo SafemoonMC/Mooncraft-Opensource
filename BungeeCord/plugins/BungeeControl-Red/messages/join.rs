29e35a71179e8d1340886bcd068a7bc3396f0157
Eduard Wayland <eduproard@gmail.com>
2022-04-21 23:12:37 +0200

    Added BungeeCord server

---
+++ BungeeCord/plugins/BungeeControl-Red/messages/join.rs
@@ -0,0 +1,9 @@
+# ---------------------------------------------------------------------------------
+# Join messages. Uses the same syntax as files in rules/ folder but operators
+# are slightly different. For documentation and a quick tutorial, see:
+# https://github.com/kangarko/ChatControl-Red/wiki/messages
+# ---------------------------------------------------------------------------------
+
+group default
+message:
+- &7{player} &fhas joined the network to {server}
