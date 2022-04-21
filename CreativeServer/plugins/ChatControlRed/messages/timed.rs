a0abac667a9c32fee1eec64fcf86e85916874fc8
Eduard Wayland <eduproard@gmail.com>
2022-04-22 00:38:05 +0200

    Added CreativeServer plugins directory

---
+++ CreativeServer/plugins/ChatControlRed/messages/timed.rs
@@ -0,0 +1,10 @@
+group default
+require sender world plotworld
+message:
+- &3Creative &8>> &fNeed help? Use &b/menu &fto open the Main Menu
+- &3Creative &8>> &fVote by using &b/vote &fand receive cool rewards
+- &3Creative &8>> &fJoin our Discord to interact with the community &bhttps://discord.gg/mooncraft &7(Click)
+- &3Creative &8>> &fMarry other players with &b/marriage
+- &3Creative &8>> &fPlay Chess with other players by using &b/warp chess
+- &3Creative &8>> &fView the WorldEdit perks for Ranked users with &b/wehelp
+- &3Creative &8>> &fYou can purchase Ranks and Keys in our online store! &bhttps://mcstore.safemoon.net/ &7(Click)
